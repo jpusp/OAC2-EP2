c3194ebe875197adf917020852d369ad9af132d3
João Paulo de Oliveira <jpos.usp@gmail.com>
2023-12-15 22:09:38 -0300

    fix script removing hardcoded paths

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -2,17 +2,17 @@
 
 thread_counts=(1 4 6 8)
 file_limits=(100 250 500 10000 20000 60000)
-xtrainFiles=("/Users/joao.paulo/dev/usp/oac2/OAC2-EP2/files/xtrain_100k.txt")
-ytrainFiles=("/Users/joao.paulo/dev/usp/oac2/OAC2-EP2/files/ytrain_100k.txt")
-xtestFile="/Users/joao.paulo/dev/usp/oac2/OAC2-EP2/files/xtest_10k.txt"
-xtest10kFile="/Users/joao.paulo/dev/usp/oac2/OAC2-EP2/files/xtest_10k.txt"
-
 cd ..
+
+xtrainFile="./files/xtrain_100k.txt"
+ytrainFile="./files/ytrain_100k.txt"
+xtestFile="./files/xtest_10k.txt"
+
 
 for threads in "${thread_counts[@]}"; do
     for limits in "${file_limits[@]}" ; do
         echo "==================================================================="
-        ./cmake-build-release/EP2 "$threads" "${xtrainFiles[0]}" "${ytrainFiles[0]}" $xtestFile $limits
+        ./cmake-build-release/EP2 "$threads" "$xtrainFile" "$ytrainFile" $xtestFile $limits
         echo ""
     done
 done
